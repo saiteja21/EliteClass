ecf0d01a0ba3588e4b4656acc893da8ddbd7bd66
Sai Teja <saitejajnvcc@gmail.com>
2017-05-31 19:41:19 +0530

    count prime in range

---
+++ PrimeCount.java
@@ -0,0 +1,8 @@
+package guvi;
+
+public class PrimeCount {
+	public static void main(String[] args) throws Exception{
+		java.io.BufferedReader read = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));   
+
+	}	
+}
