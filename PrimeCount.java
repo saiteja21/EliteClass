1bb2e35362829b9ed1420913b7d21a635f498caf
Sai Teja <saitejajnvcc@gmail.com>
2017-05-31 19:43:15 +0530

    count prime in range

--- PrimeCount.java
+++ PrimeCount.java
@@ -1,8 +1,20 @@
 package guvi;
 
-public class PrimeCount {
-	public static void main(String[] args) throws Exception{
-		java.io.BufferedReader read = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));   
+import java.math.BigInteger;
 
-	}	
+public class MainClass {
+	public static int primeCount(int m,int n){
+		int count =0;
+		for(;m<=n;m++){
+			if(new BigInteger(Integer.toString(m)).isProbablePrime(999)){
+				count++;	
+			}
+		}
+		return count;
+	}
+	public static void main(String[] args) throws Exception{
+		java.io.BufferedReader read = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+		System.out.println(primeCount(Integer.parseInt(read.readLine().toString()),Integer.parseInt(read.readLine().toString())));
+	}
 }
+
